a03c3f6ee2d55ce9989ecd28e7796e23541408c1
P3Senpai <petrkmilev@gmail.com>
2018-12-04 13:33:19 +0100

    description of robot --> 2 simultanious linear lifts lifing the cage that collects and deposits the elements. Four motor drive but no full mecanum or full omni wheel drive trains. Team marker attached to servo. description of code --> drive train uses gp1 left joystick for forward backward and turns. currently we manually lift the cage but I plan on implementing encoders to automate the process.

---
+++ launcher 2018:19/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FT_Robot.java
@@ -0,0 +1,112 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+public class FT_Robot
+{
+    /* Public OpMode members. */
+    protected DcMotor  leftDriveF   = null;
+    protected DcMotor  rightDriveF  = null;
+    protected DcMotor  leftDriveB   = null;
+    protected DcMotor  rightDriveB  = null;
+    protected DcMotor  cageLiftL    = null;
+    protected DcMotor  cageLiftR    = null;
+    protected Servo    cageIntake   = null;
+    protected DcMotor  roboLift     = null;
+    protected Servo    markerDrop    = null;
+
+    protected static final double SPEED_REDUCTION = 0.9;
+    protected static final double INTAKE_IN_SPEED = 0.1;
+    protected static final double INTAKE_OUT_SPEED = 0.9;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public FT_Robot(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+            // Drive train
+        leftDriveF  = hwMap.get(DcMotor.class, "left_drive_f");
+        rightDriveF = hwMap.get(DcMotor.class, "right_drive_f");
+        leftDriveB  = hwMap.get(DcMotor.class, "left_drive_b");
+        rightDriveB = hwMap.get(DcMotor.class, "right_drive_b");
+            // Cage Lift
+        cageLiftL   = hwMap.get(DcMotor.class, "cage_lift_l");
+        cageLiftR   = hwMap.get(DcMotor.class, "cage_lift_r");
+            // Robot lifting
+        roboLift    = hwMap.get(DcMotor.class, "robo_lift");
+
+        leftDriveF.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightDriveF.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        leftDriveB.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightDriveB.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        cageLiftL.setDirection(DcMotor.Direction.FORWARD);
+        cageLiftR.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // Set all motors to zero power
+        leftDriveF.setPower(0);
+        rightDriveF.setPower(0);
+        leftDriveB.setPower(0);
+        rightDriveB.setPower(0);
+        cageLiftL.setPower(0);
+        cageLiftR.setPower(0);
+        roboLift.setPower(0);
+
+        // Set ground positions motor positions
+//        cageLiftL.setTargetPosition(0);
+//        cageLiftR.setTargetPosition(0);
+        // TODO: add encoder init for the cage lift
+
+        //Initialize all servos
+        cageIntake  = hwMap.get(Servo.class, "cage_intake");
+        markerDrop  = hwMap.get(Servo.class, "marker_drop");
+
+        //Set servos to starting pos/pow
+        cageIntake.setPosition(0.5);
+    }
+ }
+
---
+++ launcher 2018:19/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Juju", group="Iterative Opmode")
+//@Disabled
+
+public class TeleOpMode extends OpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private FT_Robot robot =  new FT_Robot();
+
+    private String elementColour;
+    private int whiteColor;
+    private int yellowColor;
+
+     /*
+     * Code to run ONCE when the driver hits INIT
+     */
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+
+    // Set up driving so that robot can be controlled with 1 joystick (gp1, left)
+    double drive  = gamepad1.left_stick_y;
+    double turn   =  -gamepad1.left_stick_x;
+    double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+    double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+        //Left Drive Power
+    robot.leftDriveF.setPower(leftPower);    // TODO: Check if the back left motor isn't slower than the others
+    robot.leftDriveB.setPower(leftPower);
+        // Right Drive power
+    robot.rightDriveF.setPower(rightPower);
+    robot.rightDriveB.setPower(rightPower);
+
+    // Manual lift code below
+    double leftLiftPow  = Range.clip(gamepad2.left_stick_y, -1.0, 1.0);
+    double rightLiftPow = Range.clip(gamepad2.right_stick_y, -1.0, 1.0);
+
+    robot.cageLiftL.setPower(leftLiftPow);
+    robot.cageLiftR.setPower(rightLiftPow);
+
+    // cage intake code below
+    if (gamepad1.a)
+        robot.cageIntake.setPosition(robot.cageIntake.getPosition()+0.1);
+    else if (gamepad1.b)
+        robot.cageIntake.setPosition(robot.cageIntake.getPosition()-0.1);
+
+    // Robot lift controls
+        if(gamepad1.dpad_up){
+            robot.roboLift.setPower(1.0);
+        }else if(gamepad1.dpad_down){
+            robot.roboLift.setPower(-1.0);
+        } else {
+            robot.roboLift.setPower(0);
+        }
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+//        telemetry.addData("Lifting Pos", "left (%.2f) right (%.2f", weed.cageLiftL.getCurrentPosition(), weed.cageLiftR.getCurrentPosition());
+        telemetry.update();
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+    /*  TODO: find value ranges
+     * gets rgb colour values and returns: White, Yellow, Black
+     *  TODO: should i incorporate alpha() values ????????????????????
+     */
+    private String rgbValueCalc(int rawBlue, int rawGreen, int rawRed){
+        double scaleFactor = 255;
+        int cleanBlue = (int) (rawBlue * scaleFactor);
+        int cleanGreen = (int) (rawGreen * scaleFactor);
+        int cleanRed = (int) (rawRed * scaleFactor);
+
+//          TODO: find value ranges
+//        if (/*yellow*/){
+//            return "Yellow";
+//            } //else if (){
+//            return "White";
+//       }
+            return "Nothing";
+    }
+
+}
